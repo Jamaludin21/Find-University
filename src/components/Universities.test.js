0ad4096eb9d104c8dff84f2b6ae32a13135bfdb6
Jamaludin21 <jamaludinhakim21@gmail.com>
2024-07-18 21:26:23 +0700

    Adjust: unit test

--- src/components/Universities.test.js
+++ src/components/Universities.test.js
@@ -1,31 +1,52 @@
 /* eslint-disable no-undef */
-// src/components/Universities.test.js
 import { render, screen, waitFor } from "@testing-library/react";
-import axios from "axios";
-import Universities from "./Universities";
+import App from "../App";
 
-jest.mock("axios");
+const url = "http://universities.hipolabs.com/search?country=";
+const mockFetch = async () => {
+  if (url) {
+    const universities = [
+      {
+        name: "AKAD Hochschulen für Berufstätige, Fachhochschule Leipzig",
+        country: "Germany",
+        web_pages: ["http://www.akad.de/"],
+      },
+      {
+        name: "Alice-Salomon-Fachhochschule für Sozialarbeit und Sozialpädagogik Berlin",
+        country: "Germany",
+        web_pages: ["http://www.asfh-berlin.de/"],
+      },
+    ];
+
+    return {
+      ok: true,
+      json: async () => universities,
+    };
+  }
+
+  return {
+    ok: false,
+  };
+};
+
+global.fetch = jest.fn().mockImplementation(mockFetch);
 
 test("fetches and displays universities", async () => {
-  const universities = [
-    {
-      name: "Harvard University",
-      country: "United States",
-      web_pages: ["http://www.harvard.edu"],
-    },
-    {
-      name: "Stanford University",
-      country: "United States",
-      web_pages: ["http://www.stanford.edu"],
-    },
-  ];
+  render(<App />);
 
-  axios.get.mockResolvedValue({ data: universities });
+  await waitFor(() => {
+    expect(
+      screen.getByText(
+        "AKAD Hochschulen für Berufstätige, Fachhochschule Leipzig",
+        { exact: false }
+      )
+    ).toBeInTheDocument();
 
-  render(<Universities country="United States" />);
-
-  await waitFor(() =>
-    expect(screen.getByText("Harvard University")).toBeInTheDocument()
-  );
-  expect(screen.getByText("Stanford University")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "Alice-Salomon-Fachhochschule für Sozialarbeit und Sozialpädagogik Berlin",
+        { exact: false }
+      )
+    ).toBeInTheDocument();
+  });
 });
